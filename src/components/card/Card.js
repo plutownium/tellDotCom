ea14430ef04dc45ab5ca9391bf5bbf41aa5a1240
Roland Mackintosh <rolandmackintosh@outlook.com>
2021-11-15 18:10:31 -0800

    boxes are kind of styled but are ugly

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,10 +4,24 @@ import "./Card.css";
 
 function Card(props) {
   return (
-    <div className={`all ${props.completed ? "completed" : "active"}`}>
-      <div>{props.userId}</div>
-      <div>{props.title}</div>
-      <div>{props.completed ? "completed!" : "active!"}</div>
+    <div className="cardContainer">
+      <div className={`all ${props.completed ? "completed" : "active"}`}>
+        <div>
+          <p>
+            For user: <span>{props.userId}</span>
+          </p>
+        </div>
+        <div className="activityDiv">
+          <p>
+            Activity: <span>{props.title}</span>
+          </p>
+        </div>
+        <div>
+          <p>
+            Status: <span>{props.completed ? "completed!" : "active!"}</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
